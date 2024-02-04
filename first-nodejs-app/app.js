5c4d57ecb9b2bc11bce39d060d8f83f8f33c624e
patbi <biyagapatrick@gmail.com>
2024-02-04 10:01:19 +0100

    first-nodejs-program

--- first-nodejs-app/app.js
+++ first-nodejs-app/app.js
@@ -1,5 +1,15 @@
-function sayHello(title) {
-	console.log('Hello ' + title);
-}
+const http = require("http");
 
-sayHello('Founders');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
